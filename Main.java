918a612ee1be2e99dfa9638d8d2c7bdcea05af29
Ethan Luong <129918674+e10hog@users.noreply.github.com>
2025-02-18 17:32:20 +0000

    completed calcs and output formatting

--- Main.java
+++ Main.java
@@ -1,17 +1,29 @@
-// COLLABORATORS: <tutors, instructor, or anyone else who helped you>
+// COLLABORATORS: Ethan Luong
 
-// DESCRIPTION: <a sentence about what the purpose of this program is>
+// DESCRIPTION: Converts a dollar amount to quarters, dimes, nickels, and pennies.
 
 public class Main
 {
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
+    double input = 1.68;
+    int inputInPennies, quarters, dimes, nickels, pennies;
 
     //CALCULATION SECTION
 
-    //OUTPUT SECTION
+    inputInPennies = (int) (input * 100.0);
+    quarters = (int) (inputInPennies / 25);
+    inputInPennies %= 25;
+    dimes = (int) (inputInPennies / 10);
+    inputInPennies %= 10;
+    nickels = (int) (inputInPennies / 5);
+    inputInPennies %= 5;
+    pennies = (int) (inputInPennies / 1);
+    inputInPennies %= 1;
 
+    //OUTPUT SECTION
+    System.out.println(input+" can be converted to "+quarters+" quarters, "+dimes+" dimes, "+nickels+" nickels, and "+pennies+" pennies.");
   }
 
 }
